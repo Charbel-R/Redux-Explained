71110b60386f460c76bda8477631983d01d6b8e2
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-06-27 01:34:19 +0200

    prepared for presentation

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  counterReducer from "./slices/counterSlice";
-import  userReducer from "./slices/userSlice";
+
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    user: userReducer,
   },
 });
+
+
+
+
+
+
+// import  counterReducer from "./slices/counterSlice";
+// import  userReducer from "./slices/userSlice";
+
+// user: userReducer,
+// counter: counterReducer,
