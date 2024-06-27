b98c4859e2d6fc4739c6d10e64a6cd571d7ef0ae
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-06-27 02:19:35 +0200

    cleanup

--- src/components/Box2.jsx
+++ src/components/Box2.jsx
@@ -8,7 +8,7 @@ export default function Box2(props) {
 
   return (
     <div className='box'>
-      <h2>Box 2 - <span>Second Child component </span></h2>
+      <h2>Box 2 - <span>2nd child component </span></h2>
       <h3> You are {age} years old </h3>
       <Box3 user={props.user} count={props.count} />
     </div>
